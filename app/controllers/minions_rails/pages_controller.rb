210ba6539a7522fe191a3a503a0f37816c9f3361
Michael Chan <mijoch@gmail.com>
2016-02-25 10:58:17 -0800

    chore: add root route

---
+++ app/controllers/minions_rails/pages_controller.rb
@@ -0,0 +1,6 @@
+module MinionsRails
+  class PagesController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 MinionsRails::Engine.routes.draw do
+  root \
+    to: "pages#index",
+    constraints: ->(_) { Rails.env.development? || ENV['SHOW_INTERFACES_ROUTES'] }
 end
