023f82d1b5a28ac1aa56323e793c12264bb1a0bd
Michael Chan <mijoch@gmail.com>
2016-02-29 12:43:33 -0800

    feat: add documentation for CSS-development

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def css_development
+    render layout: "css_development"
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get "/pages/css_development", to: "pages#css_development"
 
   mount MinionsRails::Engine => "/minions_rails"
 end
