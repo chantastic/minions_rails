eb98d5d062ed4e9c0357106fab0fa2e3bff7a394
Michael Chan <mijoch@gmail.com>
2016-02-29 12:57:41 -0800

    feat: add documentation for CSS-production

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -5,4 +5,8 @@ class PagesController < ApplicationController
   def css_development
     render layout: "css_development"
   end
+
+  def css_production
+    render layout: "css_production"
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
   get "/pages/css_development", to: "pages#css_development"
+  get "/pages/css_production", to: "pages#css_production"
 
   mount MinionsRails::Engine => "/minions_rails"
 end
